209ac3ce3c2858e8b45da50a939c59543f967260
JhonSanz <ingjhonsanz@gmail.com>
2025-05-04 15:02:20 -0500

    Updated args parser

--- code_generator.py
+++ code_generator.py
@@ -19,13 +19,14 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="Generador de archivos hexagonales para CRUD."
     )
-    parser.add_argument("type", type=str, help="Tipo de codigo generado [auth, crud]")
+    parser.add_argument("type", type=str, help="Behaviuor: [crud, builtin].")
     parser.add_argument(
         "pascal_case", type=str, nargs="?", help="Nombre del modelo (PascalCase)."
     )
     parser.add_argument(
         "snake_case", type=str, nargs="?", help="Nombre del modelo (snake_case)."
     )
+    parser.add_argument("builtin_app", type=str, nargs="?", help="Enabled apps: [user]")
 
     args = parser.parse_args()
     builtin_generator = BuiltInGenerator()
@@ -34,7 +35,7 @@ if __name__ == "__main__":
     if args.type == "crud":
         if not args.pascal_case or not args.snake_case:
             parser.error(
-                "Para 'crud', los argumentos 'pascal_case' y 'snake_case' son obligatorios."
+                "For 'crud' option, arguments 'pascal_case' and 'snake_case' are mandatory."
             )
         code_generator = CodeGenerator(
             pascal_case=args.pascal_case,
@@ -51,8 +52,13 @@ if __name__ == "__main__":
             use_cases=APPLICATION_WEB_CASE_TEMPLATE,
             code_generator=code_generator,
         ).run()
-    elif args.type in ["auth", "role", "user"]:
+    elif args.type == "builtin":
+        if not args.builtin_app or not args.builtin_app in ["user"]:
+            parser.error(
+                f"For 'builtin' option, argument {args.builtin_app} is mandatory or not in the list."
+            )
+
         path = f"src/{args.type}"
         builtin_generator.copy_builtin_apps(path_source=path, path_target=path)
     else:
-        parser.error(f"Tipo desconocido: {args.type}")
+        parser.error(f"Unknown type: {args.type}")
